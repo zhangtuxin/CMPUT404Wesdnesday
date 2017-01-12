1271eb069f2d95174d558b674a459e717a3e9e74
TuXin Zhang <tuxin@cs.ualberta.ca>
2017-01-11 18:04:42 -0700

    work

--- checkversion.py
+++ checkversion.py
@@ -2,6 +2,6 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/zhangtuxin/CMPUT404Wesdnesday/master/checkversion.py")
 print response.text
 print response.status_code
